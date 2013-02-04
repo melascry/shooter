2bc9782a3f78f3348dcceb08257b16c1a886fb71
melascry <demontier.geoffroy@gmail.com>
2013-02-04 14:28:40 +0100

    Ennemy sprite

--- web-static/js/Ennemy.js
+++ web-static/js/Ennemy.js
@@ -7,6 +7,7 @@ var Ennemy = function(speed, hardness, root)
 	this.hardeness = hardness;
 
 	//the move sprite is too big it has only 88px of height so i resized it
+	// but the transparency seems to bug, so i'll let it that way
 	this.moveSprite = new Sprite(this.jRoot,"/shooter-static/img/move.png",2048,88,16,1,true);
 	tt = this.moveSprite;
 	this.deadSprite = new Sprite(this.jRoot,"/shooter-static/img/death.png",640,128,5,1,false);
@@ -16,29 +17,54 @@ var Ennemy = function(speed, hardness, root)
 	root.appendChild(this.jRoot.get(0));
 	
 	this.moveSprite.show();
-	this.moveSprite.play(function()
-			{
-				console.log("played one");
-			});
+	this.moveSprite.play();
 	
-	//this.jRoot.click(this.die()); On verra demain
-}
+	this.x = this.jRoot.css("top");
+	this.y = this.jRoot.css("left");
+	
+	this.desiredX = this.jRoot.css("top");
+	this.desiredY = this.jRoot.css("left");
+	
+	this.width = this.jRoot.css("width");
+	this.height = this.jRoot.css("height");
+	
+	this.timeUpdateMove = 1000/40;
+	
+	//this.jRoot.click(this.die()); //considère tout l'écran -_-
+};
 
 
 Ennemy.prototype.die = function()
 {
-	var _this = this;
+	console.log("click");
+	/*var _this = this;
 	this.moveSprite.hide();
 	this.deadSprite.show();
 	this.deadSprite.play(function(){
 		shoot.game.score+= _this.speed * _this.hardness;
 		_this.jRoot.remove();
-	});
-	
-	
-	
+	});*/
 }
+
+
 Ennemy.prototype.move = function(x,y)
 {
+	/*var _this = this;
+	if(!moving)
+		{
+		this.moving = setTimeout(function(){
+				if()
+		},
+		this.timeUpdateMove);
+	}
+	else
+	{
+		setTimeout(function()
+				{
+					_this.move(x,y);
+				},
+				this.timeUpdateMove
+				);
+	}*/
 	
 }
--- web-static/js/Menu.js
+++ web-static/js/Menu.js
@@ -1,5 +1,6 @@
 var Menu = function(score, highScore)
 {
+	var _this = this;
 	this.root = document.getElementById("menu-style");
 
 	this.jRoot = $(this.root);
@@ -12,11 +13,15 @@ var Menu = function(score, highScore)
 	this.buttonEasy = document.getElementById("easy");
 	this.buttonEasy.addEventListener("click", function()
 			{
+				_this.buttonEasy.style.className ="selected";
+				_this.buttonHard.style.className ="selectable";
 				change(1);
 			});
 	this.buttonHard = document.getElementById("hard");
 	this.buttonHard.addEventListener("click", function()
 			{
+				_this.buttonEasy.style.className ="selectable";
+				_this.buttonHard.style.className ="selected";
 				change(2);
 			});
 	
