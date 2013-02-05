11d0b76178c3242ce692da5979f74972ec0e44e5
SIG4PR <SIG4PR@PO5287.supinfogame.local>
2013-02-05 10:54:56 +0100

    xcvcvv

---
+++ web-static/js/EnnemyLife.js
@@ -0,0 +1,42 @@
+var EnnemyLife = function(speed, hardness, jParent)
+{
+	Ennemy.call(this,speed, hardness, jParent);
+	
+	this.jRoot.css("background-color", "red");
+	
+	this.flash = false;
+	this.flashing();
+};
+
+EnnemyLife.prototype = new Ennemy();
+
+EnnemyLife.prototype.dieClick = function()
+{
+	var _this = this;
+	
+	clearTimeout(this.timerMove);
+	this.timerMove = false;
+	
+	this.moveSprite.hide();
+	
+	this.deadSprite.show();
+	
+	this.deadSprite.play(function(){
+		
+		shoot.game.upLife();
+		_this.die();
+	});
+}
+
+EnnemyLife.prototype.flashing()
+{
+	var _this = this;
+	var t = 100;
+	this.jRoot.hide("fade", t, function()
+			{
+				_this.jRoot.show("fade", t, function()
+						{
+							_this.flashing();
+						});
+			});
+}
